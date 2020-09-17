122de5f72fa843d29049ecf81e323224448c2bd3
Vansh <vansharora2929@gmail.com>
2020-09-18 03:49:03 +0530

    Parsing dates

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ DATA_URL = ( "\\F:\\Data\\Motor_Vehicle_Collisions_-_Crashes.csv")
 st.title("Motor Vehichle collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be "
 "used to analyze motor vehichle collisions in NYC💥🚗")
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE','CRASH_TIME']])
+    
