ced4d0871ec5adbdc6be85f5085d66fa40cfe217
Vansh <vansharora2929@gmail.com>
2020-09-18 03:42:08 +0530

    Added path to data set

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-DATA_URL = ( "\\F:\\collision-data-analysis\\Motor_Vehicle_Collisions_-_Crashes.csv")
+DATA_URL = ( "\\F:\\Data\\Motor_Vehicle_Collisions_-_Crashes.csv")
 
 st.title("Motor Vehichle collisions in New York City")
 st.markdown("This application is a streamlit dashboard that can be "
